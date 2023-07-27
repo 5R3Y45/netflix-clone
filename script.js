d71ab22919aa2ded0f4a0548212a78b995865552
5R3Y45 <139336251+5R3Y45@users.noreply.github.com>
2023-07-27 13:03:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function formFunction() {
+    var x = document.getElementById("sign-up-form");
+    var y = document.getElementById("header-container");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+    } else {
+      x.style.display = "none";
+      y.style.display = "block";
+    }
+}
+
+
+
